fdac2bbd51c841d8d9cdc6512941e1cb9369df78
sara johnson <sarajohnson.m@gmail.com>
2023-10-04 10:12:10 -0500

    VariableChange

---
+++ WARMUPOCT4/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String personName = "John Doe";
+        int personAge = 25;
+        double productPrice = 19.99;
+        float temperatureCelsius = 27.5f;
+        boolean isLoggedIn = true;
+
+
+    }
+}
